fc9cddc3837a21f7f5c3716f2da80030b5b165e3
刘新新 <liuxinxinbit@outlook.com>
2020-09-09 08:33:22 +0800

    1111

--- model/pspnet.py
+++ model/pspnet.py
@@ -20,39 +20,26 @@ from .net_parts import build_conv2D_block, build_conv2Dtranspose_block,bottlenec
 def BN():
     return BatchNormalization(momentum=0.95, epsilon=1e-5)
 
-def residual_conv(prev, level, pad=1, lvl=1, sub_lvl=1, modify_stride=False):
+def residual_conv(prev, level, modify_stride=False):
     if modify_stride is False:
         prev = build_conv2D_block(prev, filters=64 * level, kernel_size=1, strides=1)
     elif modify_stride is True:
         prev = build_conv2D_block(prev, filters=64 * level, kernel_size=1, strides=2)
-
-    prev = BN()(prev)
-    prev = Activation('relu')(prev)
-
-    prev = ZeroPadding2D(padding=(pad, pad))(prev)
-    prev = Conv2D(64 * level, (3, 3), strides=(1, 1), dilation_rate=pad, use_bias=False)(prev)
-
-    prev = BN()(prev)
-    prev = Activation('relu')(prev)
+    prev = build_conv2D_block(prev, filters=64 * level, kernel_size=3, strides=1,dilation_rate=(1, 1))
     prev = Conv2D(256 * level, (1, 1), strides=(1, 1), use_bias=False)(prev)
     prev = BN()(prev)
     return prev
 
 
-def short_convolution_branch(prev, level, lvl=1, sub_lvl=1, modify_stride=False):
-    lvl = str(lvl)
-    sub_lvl = str(sub_lvl)
-    names = ["conv" + lvl + "_" + sub_lvl + "_1x1_proj",
-             "conv" + lvl + "_" + sub_lvl + "_1x1_proj_bn"]
+def short_convolution_branch(prev, level, modify_stride=False):
 
     if modify_stride is False:
-        prev = Conv2D(256 * level, (1, 1), strides=(1, 1), name=names[0],
+        prev = Conv2D(256 * level, (1, 1), strides=(1, 1),
                       use_bias=False)(prev)
     elif modify_stride is True:
-        prev = Conv2D(256 * level, (1, 1), strides=(2, 2), name=names[0],
+        prev = Conv2D(256 * level, (1, 1), strides=(2, 2),
                       use_bias=False)(prev)
-
-    prev = BN(name=names[1])(prev)
+    prev = BN()(prev)
     return prev
 
 
@@ -60,24 +47,20 @@ def empty_branch(prev):
     return prev
 
 
-def residual_short(prev_layer, level, pad=1, lvl=1, sub_lvl=1, modify_stride=False):
+def residual_short(prev_layer, level, modify_stride=False):
     prev_layer = Activation('relu')(prev_layer)
     block_1 = residual_conv(prev_layer, level,
-                            pad=pad, lvl=lvl, sub_lvl=sub_lvl,
                             modify_stride=modify_stride)
 
     block_2 = short_convolution_branch(prev_layer, level,
-                                       lvl=lvl, sub_lvl=sub_lvl,
                                        modify_stride=modify_stride)
     added = Add()([block_1, block_2])
     return added
 
 
-def residual_empty(prev_layer, level, pad=1, lvl=1, sub_lvl=1):
+def residual_empty(prev_layer, level):
     prev_layer = Activation('relu')(prev_layer)
-
-    block_1 = residual_conv(prev_layer, level, pad=pad,
-                            lvl=lvl, sub_lvl=sub_lvl)
+    block_1 = residual_conv(prev_layer, level)
     block_2 = empty_branch(prev_layer)
     added = Add()([block_1, block_2])
     return added
@@ -110,7 +93,34 @@ class pspnet:
         cnv1 = build_conv2D_block(cnv1, filters=64, kernel_size=3, strides=1)
         res = MaxPooling2D(pool_size=(3, 3), padding='same', strides=(2, 2))(cnv1)
 
+        # 2_1- 2_3
+        res = residual_short(res, 1, pad=1)
+        for i in range(2):
+            res = residual_empty(res, 1, pad=1, lvl=2, sub_lvl=i + 2)
 
+    # 3_1 - 3_3
+    res = residual_short(res, 2, pad=1, lvl=3, sub_lvl=1, modify_stride=True)
+    for i in range(3):
+        res = residual_empty(res, 2, pad=1, lvl=3, sub_lvl=i + 2)
+    if layers is 50:
+        # 4_1 - 4_6
+        res = residual_short(res, 4, pad=2, lvl=4, sub_lvl=1)
+        for i in range(5):
+            res = residual_empty(res, 4, pad=2, lvl=4, sub_lvl=i + 2)
+    elif layers is 101:
+        # 4_1 - 4_23
+        res = residual_short(res, 4, pad=2, lvl=4, sub_lvl=1)
+        for i in range(22):
+            res = residual_empty(res, 4, pad=2, lvl=4, sub_lvl=i + 2)
+    else:
+        print("This ResNet is not implemented")
+
+    # 5_1 - 5_3
+    res = residual_short(res, 8, pad=4, lvl=5, sub_lvl=1)
+    for i in range(2):
+        res = residual_empty(res, 8, pad=4, lvl=5, sub_lvl=i + 2)
+
+    res = Activation('relu')(res)
 
 
 
